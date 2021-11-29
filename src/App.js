7112c3d99bf6a0f047979bfc2fae583e732d66c0
Viet An Truong <vietan124@gmail.com>
2021-11-28 18:11:45 -0700

    cleaned up App.js & deleted CurrentIssues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import AddIssue from './Components/AddIssue/AddIssue';
-import CurrentIssue from './Components/CurrentIssue/CurrentIssue';
 import BugList from './Components/BugList/BugList';
 import './App.css';
 
 function App() {
-  const [addIssue, setAddIssue] = useState(true)
   const [bugs, setBugs] = useState([])
 
   const loadBugs = async () => {
@@ -21,23 +19,6 @@ function App() {
   useEffect(() => {
     loadBugs();
   }, []);
-
-  // return (
-  //   <div className="App">
-  //     <h1>Bug Tracker</h1>
-  //     <div className="grey-card-container">
-  //       {addIssue ? <AddIssue /> : <CurrentIssue />}
-  //     </div>
-  //     <div className="view-selection">
-  //       <button class="view-button" onClick={() => setAddIssue(false)}>
-  //         Current Issues
-  //       </button>
-  //       <button class="view-button active-button" onClick={() => setAddIssue(true)}>
-  //         Add Issue
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
 
   return (
     <div className="App">
